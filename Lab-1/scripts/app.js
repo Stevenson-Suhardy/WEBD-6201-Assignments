c389a5899578a1c7b3c16048e95e3f06f1c9224e
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-01-27 12:34:40 -0500

    Added welcome page in index and name, phone number, and message on contact form

--- Lab-1/scripts/app.js
+++ Lab-1/scripts/app.js
@@ -1,7 +1,13 @@
 function timedRedirect(e) {
+  let fullName = document.getElementById("fullName").value;
   let contactEmail = document.getElementById("emailInput").value;
+  let contactNumber = document.getElementById("contactNumber").value;
+  let message = document.getElementById("message").value;
 
+  console.log("Name: " + fullName);
   console.log("Email: " + contactEmail);
+  console.log("Phone Number: " + contactNumber);
+  console.log("Message: " + message);
   e.preventDefault();
   setTimeout(function () {
     window.location.href = "index.html";
