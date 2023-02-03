15f8a28d647cc113d4bec6b819787a61c435653b
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-03 16:44:14 -0500

    Completed lab 1

--- Lab-1/scripts/app.js
+++ Lab-1/scripts/app.js
@@ -1,28 +1,48 @@
+/**
+ * Name: Stevenson Suhardy
+ * Student ID: 100839397
+ * Date Completed: February 3, 2023
+ */
+
+// Get the list element inside the navbar element
 let ul = document.getElementsByClassName("navbar-nav ml-auto")[0];
 
+// Gets the list items inside the list
 let product = document.getElementsByClassName("nav-link")[1];
+// Changing the innerHTML to an icon and interests
 product.innerHTML = "<i class=\"fa-solid fa-layer-group\"></i> Interests";
 
+// Creating a new list item with the appropriate class
 let hR = document.createElement("li");
 hR.className = "nav-item";
 
+// Creating a new 'a' element along with the class, href, and innerHTML
 let hRLink = document.createElement("a");
 hRLink.className = "nav-link";
 hRLink.href = "./humanResources.html";
 hRLink.innerHTML = "<i class=\"fa-solid fa-users\"></i> Human Resources";
 
+// Inserting the link inside the list item
 hR.appendChild(hRLink);
+// Insert the hR list item before the contact us list item
 ul.insertBefore(hR, ul.children.item(4));
 
+// Creating footer
+// Creating a new navbar for footer along with the class
 let navFooter = document.createElement("nav");
 navFooter.className = "navbar fixed-bottom navbar-light bg-light";
 
+// Creating div for the footer
 let divFooter = document.createElement("div");
 divFooter.className = "container-fluid";
 
+// Creating the text for the footer
 let footerText = document.createElement("p");
 footerText.innerHTML = "&copy; Copyright 2023";
 
+// Appending the text to the div
 divFooter.appendChild(footerText);
+// Appending the div to the nav
 navFooter.appendChild(divFooter);
+// Appending the nav to the body
 document.body.appendChild(navFooter);
--- Lab-1/scripts/contact.js
+++ Lab-1/scripts/contact.js
@@ -1,26 +1,43 @@
+/**
+ * Name: Stevenson Suhardy
+ * Student ID: 100839397
+ * Date Completed: February 3, 2023
+ */
+
+/**
+ * This function will console logs all the data that the user inputs when the form is valid, and redirects them to the home page after 3 seconds.
+ * @param {Event} e 
+ */
 function timedRedirect(e) {
+  // Gets all the input element
   let fullName = document.getElementById("fullName").value;
   let contactEmail = document.getElementById("emailInput").value;
   let contactNumber = document.getElementById("contactNumber").value;
   let message = document.getElementById("message").value;
 
+  // Gets the form element
   let form = document.getElementById("contactForm");
-
+  // Checking to see if the form is valid
   if (form.checkValidity()) {
+    // Prevent form from being submitted
     e.preventDefault();
-
+    // Console logs the data
     console.log("Name: " + fullName);
     console.log("Email: " + contactEmail);
     console.log("Phone Number: " + contactNumber);
     console.log("Message: " + message);
     
+    // Redirect to index.html after 3 seconds
     setTimeout(function () {
       window.location.href = "index.html";
     }, 3000);
   }
 }
 
+// Getting the submit button
 let submitButton = document.getElementById("submit-button");
+// Checking to see if the submitButton exists
 if (submitButton) {
+  // Adding an event listener when the button is clicked
   submitButton.addEventListener("click", timedRedirect, false);
 }
