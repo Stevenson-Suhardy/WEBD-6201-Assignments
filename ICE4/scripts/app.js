738ea351cedf48d7f3c2a512b671f65531a33db9
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-09 14:26:09 -0500

    Added slideshow page

--- ICE4/scripts/app.js
+++ ICE4/scripts/app.js
@@ -16,8 +16,9 @@ $(function () {
 	                </nav>
                 </div>`
 
-    //TO DO: replace the nav-holder
-
+    // TO DO: replace the nav-holder
+    // DONE
+$("#nav-holder").replaceWith(navBar);
 });
 
 /**
@@ -31,50 +32,70 @@ $(function () {
 /**
  * fuction to demo adding content with .text() and .html()
  *  */
-// $(function () {
+$(function () {
 
-    // let navDiv = $("div:first");
+    let navDiv = $("div:first");
     // console.log(navDiv.html())
     // console.log(navDiv.text())
 
-    // let contentDiv = $("#content-div");
+    let contentDiv = $("#content-div");
     // console.log(contentDiv.html())
     // console.log(contentDiv.text())
 
     // text to use with new paragraph
-    // text = "It is one of his best works!"
+    let text = "It is one of his best works!"
     // create a paragraph
-    // let newP = $("<p></p>");
+    let newP = $("<p></p>");
     // TO DO: add text with html()
+    newP.text(text);
 
     // append to contentDiv
+    contentDiv.append(newP);
 
     // add text
-    // newP.text("I am very excited for the opening of the new adaptation of his work.");
+    newP.text("I am very excited for the opening of the new adaptation of his work.");
     // TO DO: test when very excited is in a strong tag
+    newP.html("I am <strong>very excited</strong> for the opening of the new adaptation of his work.");
 
     // TO DO: append to newP text
 
-// });
+});
 
 /**
  * function to demo adding toggle to button
  *  */
-// $(function () {
+$(function () {
     // TO DO: complete the function
     // get the button
+    let btnHide = $("#btnDivToggle");
     // add a click function
+    btnHide.click(function() {
         // get the parent div's p tags
+        let paras = $(".toggleDiv").find('p');
         // for each p in the div
+        $(paras).each(function() {
+            // This toggle method does depending on the css. Use the if statements for more free control
+            // $(this).toggle();
             // if it has toggleHide class
+            if ($(this).hasClass("toggleHide")) {
                 //remove toggleHide class and add toggleShow class
+                $(this).removeClass("toggleHide").addClass("toggleShow");
                 // styling is controlled in the css
+            } 
             // otherwise assume it has the toggleShow class
+            else {
                 //remove toggleShow and add toggleHide
+                $(this).removeClass("toggleShow").addClass("toggleHide");
+            }
+        });
+    });
+});
 
-//  });
-
-
+// function to add the image next to the text
+if($("#speckled-excerpt")) {
+    let aspImg = "<img class='asp-img' src='./images/asp.jpeg' />";
+    $(aspImg).insertBefore($("#speckled-excerpt"));
+}
 
 // FORM JQUERY
 // TO DO: import form validation functions with alias
@@ -125,52 +146,63 @@ $(function () {
 
 // SLIDESHOW
 // TO DO: if there's a gallery class on the page
-
-    // call a callback function to handle the galler rotation
+if($(".gallery")) {
+    $(function() {
+        // call a callback function to handle the galler rotation
 
 
         // get the image tag
+        let galleryImage = $(".gallery").find("img").first();
         // get a list of your images
-        // let images = [
-        //     "./images/portraits/portrait-01.jpg",
-        //     "./images/portraits/portrait-02.jpg",
-        //     "./images/portraits/portrait-03.jpg",
-        //     "./images/portraits/portrait-04.jpg",
-        //     "./images/portraits/portrait-05.jpg",
-        //     "./images/portraits/portrait-06.jpg",
-        //     "./images/portraits/portrait-07.jpg",
-        //     "./images/portraits/portrait-08.jpg",
-        //     "./images/portraits/portrait-09.jpg",
-        //     "./images/portraits/portrait-10.jpg",
-        //     "./images/portraits/portrait-11.jpg",
-        //     "./images/portraits/portrait-12.jpg",
-        //     "./images/portraits/portrait-13.jpg",
-        //     "./images/portraits/portrait-14.jpg",
-        //     "./images/portraits/portrait-15.jpg",
-        //     "./images/portraits/portrait-16.jpg",
-        //     "./images/portraits/portrait-17.jpg",
-        //     "./images/portraits/portrait-18.jpg",
+        let images = [
+            "./images/portraits/portrait-01.jpg",
+            "./images/portraits/portrait-02.jpg",
+            "./images/portraits/portrait-03.jpg",
+            "./images/portraits/portrait-04.jpg",
+            "./images/portraits/portrait-05.jpg",
+            "./images/portraits/portrait-06.jpg",
+            "./images/portraits/portrait-07.jpg",
+            "./images/portraits/portrait-08.jpg",
+            "./images/portraits/portrait-09.jpg",
+            "./images/portraits/portrait-10.jpg",
+            "./images/portraits/portrait-11.jpg",
+            "./images/portraits/portrait-12.jpg",
+            "./images/portraits/portrait-13.jpg",
+            "./images/portraits/portrait-14.jpg",
+            "./images/portraits/portrait-15.jpg",
+            "./images/portraits/portrait-16.jpg",
+            "./images/portraits/portrait-17.jpg",
+            "./images/portraits/portrait-18.jpg",
 
-        // ]
+        ]
 
 
 
         // set a first index
-
+        let imageIndex = 0;
         // call the setInterval method that will re-call this method at a set interval
-
+        setInterval(function() {
             // increment the image index but no greater than how many images you have
-
+            imageIndex = (imageIndex + 1) % images.length;
             // fade out the current image
-
+            galleryImage.fadeOut(1000, function() {
                 // $(this) refers to the object that calls the callback or in this case galleryImage
+                
                 // change the src attribute of the image
-
+                $(this).attr("src", images[imageIndex]);
                 // fade it back in
-
+                $(this).fadeIn(1000);
                 // debug statement
+                console.log($(this).attr("src"));
+            })
+                
 
             //set the time for more than how long the fade out and in process will take or you won't get the images you expect
+        }, 3000);
+    }());
+}
+
+    
 
 
 
