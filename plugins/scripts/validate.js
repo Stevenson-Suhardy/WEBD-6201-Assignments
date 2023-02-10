be01d15a1c7e8770b3cbc47e67fe7ff45f4794dd
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-10 11:49:33 -0500

    Added JQuery Plugins

---
+++ plugins/scripts/validate.js
@@ -0,0 +1,30 @@
+$("#MyForm").validate();
+
+
+// $(document).ready(function () {
+// 				$("#MyForm").validate({
+// 					rules: {
+// 						FName: {
+// 							required: true,
+// 							minlength: 2
+// 						},
+// 						LName: {
+// 							required: true,
+// 							minlength: 2
+// 						},
+// 						Email: {
+// 							required: true,
+// 							email: true
+// 						},
+// 						Pwd: {
+// 							required: true
+// 						}
+// 					},
+// 					// messages: {
+// 					// 	FName: "Please enter a valid First Name.",
+// 					// 	LName: "Please enter a valid Last Name.",
+// 					// 	Email: "Please enter Email in proper format.",
+// 					// 	Pwd: "Please enter a Password"
+// 					// }
+// 				});
+// 			});
