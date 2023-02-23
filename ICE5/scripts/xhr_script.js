abe54022335d3b4cc1b0bffcbe812ea3e5d342b2
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-23 14:31:33 -0500

    Completed ICE 5

--- ICE5/scripts/xhr_script.js
+++ ICE5/scripts/xhr_script.js
@@ -8,22 +8,45 @@ let url_todos = 'https://jsonplaceholder.typicode.com/todos';
 let url_users = 'https://jsonplaceholder.typicode.com/users';
 
 // instantiate the XMLHttpRequest object with the new keyword
-
+let xhr = new XMLHttpRequest;
 
 
 // create a callback function to fire when the onreadystatechange happens
+xhr.onreadystatechange = () => {
     // check that the state is done
-    // if (xhr.readyState === 4) {
+    console.log(`The state: ${xhr.readyState}`);    
+    // if (xhr.readyState === XMLHttpRequest.DONE) {
+    if (xhr.readyState === 4) {
+
+        if (xhr.status === 200) {
+        // console.log(`The response: ${xhr.responseText}`);
             // turn into json
+            let jsonResponse = JSON.parse(xhr.responseText);
             // console log to see what we have
+            console.log(`The response: ${jsonResponse[0]["url"]}`);
+            let photo_url_1 = jsonResponse[0]["url"];
+            let photo_url_2 = jsonResponse[1]["url"];
             // update the 1st image
+            $("#photo1").attr("src", photo_url_1);
             // update the 1st figcaption
+            $("#photo1 ~ figcaption").text("First image is updated using AJAX call");
             // update the 2nd image
+            $("#photo2").attr("src", photo_url_2);
             // update the 2nd figcaption
+            $("#photo2 ~ figcaption").text("Second image is updated using AJAX call");
+        }
+        else {
             // send error message
+            alert(`NOPE! Did not work - returned with status code ${xhr.status}.`);
+        }
+    }
+}
+    
 
 // use the .open() method to configure the object
+xhr.open("GET", url_photos);
 
 // add datatype to header
 
 // use the .send() method to send the request
+xhr.send();
