5429f9b6f406f36f93e655279f6b027201a3a79d
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-02-24 12:12:15 -0500

    Completed ICE 6

--- ICE6/scripts/fetch_script.js
+++ ICE6/scripts/fetch_script.js
@@ -1,16 +1,31 @@
 console.log("fetch_script.js loaded");
 
 // get the button for fetch
-
+let btnFetch = $("#fetchJoke");
 // create a url variable
-
+let urlFetch = "https://icanhazdadjoke.com/";
 //need a header Accept value set to 'application/json'
+let myFetchHeaders = {"Accept": "application/json"};
 
 // create the callback for the click
+$(btnFetch).click(() => {
     // use fetch
-        // give the necessary header data
+    fetch(
+        urlFetch, {
+            // give the necessary header data
+            headers: myFetchHeaders
+        })
         // first then() to recieve the promise
-            // send the json from the promise on to the next then()
+        .then((res) => {
+            return res.json();
+        // send the json from the promise on to the next then()
+        })
+        .then((jsonRes) => {
             // console log the json
+            console.log(jsonRes);
             // set the output
+            $("#output").text(jsonRes["joke"]);
+        })
+})
+    
 
--- ICE6/scripts/xhr_script.js
+++ ICE6/scripts/xhr_script.js
@@ -1,19 +1,39 @@
 console.log("xhr_script.js loaded");
 
 // get the button for xhr
+let btnXhr = $("#xhrJoke");
 
 // create a url variable
+let urlXhr = "https://icanhazdadjoke.com/"; 
 
 // create a click callback that handles the API call
+$(btnXhr).click(() => {
     // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest;
     // confiture with open()
+    xhr.open("GET", urlXhr);
     // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
     // create the callback to handle the onreadystatechange
+    xhr.onreadystatechange = () => {
         // only execute when done and status is 200
+        if (xhr.status === 200 && xhr.readyState === 4) {
             // get the responseText
+            let joke = xhr.responseText;
             // console log
+            console.log(joke);
             // JSON.parse the joke
+            let jsonResponse = JSON.parse(joke);
             // set the output
+            $("#output").text(jsonResponse["joke"]);
+        }
+            
+    }
+    
     // send the request
+    xhr.send();
+        
+})
+    
 
 
