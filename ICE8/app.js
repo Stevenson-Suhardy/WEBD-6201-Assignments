73600c558b978801f038d468a499c3594ce483b4
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-03-17 12:12:39 -0400

    ICE 8 - Added views

--- ICE8/app.js
+++ ICE8/app.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const PORT = 3000;
 
 // create an express app
 const app = express();
 
+// set up the Handlebars engine
+app.engine("handlebars", exphbs.engine({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use((req, res, next) => {
+    console.log(`URL: ${req.url}`);
+    req.myName = "Stevenson"
+
+    next();
+})
+
+// index route
 app.get('/', (req, res) => {
-    res.send("Hello, WEBD6201 World! Now improved with NODEMON");
+    res.send(`Hello ${req.myName}, WEBD6201 World! Now improved with NODEMON`);
+})
+
+app.post("/registration", (req, res) => {
+    res.send(`This is the registration page.`);
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is the about page for WEBD6201.");
 })
 
 
