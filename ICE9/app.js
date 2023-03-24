94b3b2a272af39a1a7fdb3a20c750ed6f087aa3e
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-03-24 00:29:52 -0400

    Added ICE9

---
+++ ICE9/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const PORT = 3000;
+
+// create an express app
+const app = express();
+
+// set up the Handlebars engine
+app.engine("handlebars", exphbs.engine({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use((req, res, next) => {
+    console.log(`URL: ${req.url}`);
+    req.myName = "Stevenson"
+
+    next();
+})
+
+// index route
+app.get('/', (req, res) => {
+    // render the home.handlebars
+    res.render("home");
+})
+
+// about route
+app.get("/about", (req, res) => {
+    res.send("This is the about page for WEBD6201.");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+
+// run the server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
